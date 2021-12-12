184e534cc5c75d87796ad37fb145eb54770af8a5
yakzi <zieba.jakub.97@gmail.com>
2021-12-12 19:14:30 +0100

    ADD Post model

---
+++ AnyPost/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnyPost.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PostDate { get; set; }
+        public int Rating { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Tags { get; set; }
+
+    }
+}
