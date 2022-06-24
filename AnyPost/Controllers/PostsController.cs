5e46ef6a44539302f0c3c9fd62a9c2624cc049ea
Jakub Zięba <zieba.jakub.97@gmail.com>
2022-06-24 22:12:31 +0200

    MODIFY some cleanup of this total mess.

--- AnyPost/Controllers/PostsController.cs
+++ AnyPost/Controllers/PostsController.cs
@@ -60,7 +60,6 @@ namespace AnyPost.Controllers
             _context.Add(Item3);
             await _context.SaveChangesAsync();
             return RedirectToAction("Details", new { id = Item3.PostId });
-            //RedirectToAction()
         }
 
         // GET: Posts/Create
@@ -69,10 +68,7 @@ namespace AnyPost.Controllers
         {
             return View();
         }
-
-        // POST: Posts/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.      
+    
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Content,PostDate,Rating,UserId,UserName,Tags")] Post post)
@@ -88,78 +84,7 @@ namespace AnyPost.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(post);
-        }
-
-        // GET: Posts/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var post = await _context.Post.FindAsync(id);
-            if (post == null)
-            {
-                return NotFound();
-            }
-            return View(post);
-        }
-
-        // POST: Posts/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,PostDate,Rating,UserId,UserName,Tags")] Post post)
-        {
-            if (id != post.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(post);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PostExists(post.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View();
-        }
-
-        // GET: Posts/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var post = await _context.Post
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (post == null)
-            {
-                return NotFound();
-            }
-
-            return View(post);
-        }
-
-        
+        }        
         public IActionResult RatingUp(int Id)
         {
             var record = _context.Post.Single(record => record.Id == Id);
@@ -174,22 +99,6 @@ namespace AnyPost.Controllers
             record.Rating--;
             _context.SaveChanges();
             return RedirectToAction(nameof(Index));
-        }
-
-        // POST: Posts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var post = await _context.Post.FindAsync(id);
-            _context.Post.Remove(post);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool PostExists(int id)
-        {
-            return _context.Post.Any(e => e.Id == id);
         }
     }
 }
